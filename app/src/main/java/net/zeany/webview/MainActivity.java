03d61031f17662b88f57cd7aa8f345e667b5cbbe
EomjunYoung <theoneday737@naver.com>
2018-01-12 12:15:38 +0900

    add firebase

--- app/src/main/java/net/zeany/webview/MainActivity.java
+++ app/src/main/java/net/zeany/webview/MainActivity.java
@@ -2,12 +2,21 @@ package net.zeany.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv);
+
+        tv.setText("앱테스트용");
+
     }
 }
